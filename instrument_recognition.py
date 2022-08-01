0fe2e37948f12857132e9d235bfbf7ee998071c7
deJilz <cdejohn7@gmail.com>
2022-08-01 16:13:33 -0400

    got it working in env

--- instrument_recognition.py
+++ instrument_recognition.py
@@ -4,26 +4,24 @@ import os
 import pytesseract
 import re
 
-textImgs_path = './ Dataset / images /'
+textImgs_path = 'Dataset/images/'
 path_list = os.listdir ( textImgs_path )
 
 all_instruments_counter = 0
 for img_path in path_list :
-    if img_path.split ('.') [ -1] == 'png ':
+    if img_path.split ('.') [ -1] == 'png':
         # name of the image in the folder , e.g. 20
         img_name = img_path.split ('.') [0]
 
-        # the name of the output filefor e.g. image name is 20. png ,
-        file name is 20. txt
-        file_path = './ Dataset / images / circles_txt /'+ img_name +'. txt '
+        # the name of the output filefor e.g. image name is 20. png , file name is 20. txt
+        file_path = 'Dataset/images/circles_txt/'+ img_name +'.txt'
         file = open ( file_path , 'w')
         file.write ('instrument x y r:\n')
         img = cv2.imread ( textImgs_path + img_path )
         gray = cv2.cvtColor ( img , cv2.COLOR_BGR2GRAY )
 
         # Hough Circle Transform with OpenCV
-        circles1 = cv2.HoughCircles ( gray , cv2.HOUGH_GRADIENT , 1 ,
-        10 , param1 =100 , param2 =60 , minRadius =10 , maxRadius =30)
+        circles1 = cv2.HoughCircles ( gray , cv2.HOUGH_GRADIENT , 1 , 10 , param1 =100 , param2 =60 , minRadius =40, maxRadius =60)
         circles = circles1 [0 , : , :]
         circles = np.uint16 ( np.around ( circles ) )
         counter = 0
@@ -31,9 +29,9 @@ for img_path in path_list :
         # draw the detected circles on the original image and show
         for i in circles [:]:
             cv2.circle ( img , ( i [0] , i [1]) , i [2] , (255 , 0 , 255) , 7)
-        cv2.imshow ('detected circles ', img )
-        cv2.waitKey ()
-        # cv2. imwrite ( './ Dataset / images / circles / '+ img_name + '. png', img )
+        #cv2.imshow ('detected circles ', img )
+        #cv2.waitKey ()
+        cv2.imwrite ( 'Dataset/images/circles/'+ img_name + '.png', img )
 
         for i in circles [:]:
             cv2.circle ( img , ( i [0] , i [1]) , i [2] , (255 , 255 , 255) , 7)
@@ -42,30 +40,31 @@ for img_path in path_list :
             if i [2] >= 25:
                 counter += 1
                 # generate the candidate area
-                cropped_image_name = './ Dataset / images / circles / id_ ' + img_name + '/ circle_ ' + str( counter ) + '. png '
+                cropped_image_name = 'Dataset/images/circles/id_' + img_name + '/circle_' + str( counter ) + '.png'
                 cropped_circles = img [( i [1] - i [2]) :( i [1] + i [2]) , ( i [0] - i [2]) :( i [0] + i [2]) ]
 
                 # use Tesseract
                 cropped_circles_rgb = cv2.cvtColor ( cropped_circles ,cv2.COLOR_BGR2RGB )
                 # Recognition is very sensitive to this configuration
                 custom_config = r'--oem 3 --psm 6'
-                text = pytesseract.image_to_string (
-                cropped_circles_rgb , config = custom_config )
+                text = pytesseract.image_to_string (cropped_circles_rgb , config = custom_config )
+                
+                
                 # Postprocessing Steps :
                 # 1. write in one line
-                text = text.replace ('\n', ' ').replace ('\r',' ')
+                text = text.replace ('\n', '').replace ('\r','')
                 # 2. Uppercase
                 text = text.upper ()
                 # 3. only keep the upper - case abbr and numbers
-                text = re.sub (u" ([^\ u0030 -\ u0039 \u0041 -\ u005a \ u002e])", "", text )
+                text = re.sub (u" ([^\ u0030 -\ u0039 \u0041 -\ u005a \ u002e])", "", text) # literall what
                 # 4. Some common problem ( specific to this dataset )
                 text = text.replace ('TL ', 'TI ')
                 text = text.replace ('POIE ', 'PDIE ')
 
-                # print ( text )
+                print ( text )
 
-                cv2.imshow (" circles ", cropped_circles )
-                cv2.waitKey ()
+                #cv2.imshow (" circles ", cropped_circles )
+                #cv2.waitKey ()
                 # print ( ' - - - - - - - - - - - - - next circle - - - - - - - - - - - - - - - - - ')
                 file.write ( text +' ') # instrument
                 file.write (str ( i [0]) +' ') # center point x
--- pdf2image.py
+++ pdf2image.py
@@ -6,31 +6,32 @@ def pdf_image ( pdfPath , imgPath , zoom_x , zoom_y , rotation_angle ) :
     # open pdf file
     pdf = fitz.open ( pdfPath )
 
-    dir_path_obj = './ Dataset / images /'
+    dir_path_obj = 'Dataset/images/'
     files = os.listdir ( dir_path_obj )
     # read each page
-    for pg in range (0 , pdf.pageCount ) :
+    for pg in range (0 , pdf.page_count ) :
         page = pdf [ pg ]
         # zoom_x = zoom_y =3 -> use 3 * original resolution
-        trans = fitz.Matrix ( zoom_x , zoom_y ).preRotate (rotation_angle)
-        pm = page.getPixmap ( matrix = trans , alpha = False )
+        trans = fitz.Matrix ( zoom_x , zoom_y ).prerotate (rotation_angle)
+        pm = page.get_pixmap ( matrix = trans , alpha = False )
         rename = pg + len ( files )
         # write the image
-        pm.writePNG ( imgPath +str ( rename ) +".png")
+        pm.save(imgPath +str ( rename ) +".png")
     pdf.close ()
 
 def main ( _argv ) :
     # folder storing the PDF files
-    dir_path = './ Dataset '
-    files = os.listdir ( dir_path )
+    dir_path = 'Dataset/'
+    files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path,f))]
+    print("files",files)
     for file in files :
         pdfPath = dir_path +'/'+ file
         # print ( pdfPath )
-        pdf_image ( pdfPath , './ Dataset / images /', 3 , 3 , 0)
+        pdf_image ( pdfPath , 'Dataset/images/', 3 , 3 , 0)
     print (" finished ")
 
 if __name__ == '__main__':
     try:
-        main()
+        app.run(main)
     except SystemExit :
         pass
