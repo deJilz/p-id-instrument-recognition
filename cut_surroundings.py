76887b4f1f34b18757a35002eedd10fe14cd1b61
deJilz <cdejohn7@gmail.com>
2022-08-01 14:41:54 -0400

    fixed indentations and added folders

--- cut_surroundings.py
+++ cut_surroundings.py
@@ -5,31 +5,31 @@ def crop_image ( file_path , dest_path ) :
     files = os . listdir ( file_path ) # list all the files ' names
     counter = 0
     for file in files :
-    # print ( file )
-     original_image_name = os . path . join ( file_path , file )
-     # dest_image_name = os. path . join ( dest_path , file )
-    dest_image_name = dest_path + str( counter ) + '. png '
-    if os . path . isfile ( original_image_name ) :
-    print ( original_image_name )
-    original_image = cv2 . imread ( original_image_name )
-    shape = original_image . shape
-    h = shape [0] # Y ( height ) = 1786
-    w = shape [1] # X ( width ) = 2526
-    print ("h = ", h )
-    print ("w = ", w )
-    cropped_y_start = int( h * 0.3) # y start
-    cropped_y_end = int( h * 0.85) # y end
-    cropped_x_start = int( w * 0.05) # x start
-    cropped_x_end = int( w * 0.95) # x end
-    cropped_img = original_image [ cropped_y_start :
-    cropped_y_end , cropped_x_start : cropped_x_end ] # crop
-    # print ( dest_image_name )
-    # cv2. imshow ( ' cropped ', cropped_img )
-    # cv2. waitKey ()
+        # print ( file )
+         original_image_name = os . path . join ( file_path , file )
+         # dest_image_name = os. path . join ( dest_path , file )
+        dest_image_name = dest_path + str( counter ) + '. png '
+        if os . path . isfile ( original_image_name ) :
+            print ( original_image_name )
+            original_image = cv2 . imread ( original_image_name )
+            shape = original_image . shape
+            h = shape [0] # Y ( height ) = 1786
+            w = shape [1] # X ( width ) = 2526
+            print ("h = ", h )
+            print ("w = ", w )
+            cropped_y_start = int( h * 0.3) # y start
+            cropped_y_end = int( h * 0.85) # y end
+            cropped_x_start = int( w * 0.05) # x start
+            cropped_x_end = int( w * 0.95) # x end
+            cropped_img = original_image [ cropped_y_start :
+            cropped_y_end , cropped_x_start : cropped_x_end ] # crop
+            # print ( dest_image_name )
+            # cv2. imshow ( ' cropped ', cropped_img )
+            # cv2. waitKey ()
 
-    cv2 . imwrite ( dest_image_name , cropped_img ) #
-    counter +=1
-    # print ( dest_image_name )
+            cv2 . imwrite ( dest_image_name , cropped_img ) #
+            counter +=1
+            # print ( dest_image_name )
 
 if __name__ == '__main__':
     crop_image ('./ Dataset / images /', './ Dataset / images / results /')
--- pdf2image.py
+++ pdf2image.py
@@ -4,30 +4,29 @@ import os
 
 def pdf_image ( pdfPath , imgPath , zoom_x , zoom_y , rotation_angle ) :
     # open pdf file
-    pdf = fitz . open ( pdfPath )
+    pdf = fitz.open ( pdfPath )
 
     dir_path_obj = './ Dataset / images /'
-    files = os . listdir ( dir_path_obj )
+    files = os.listdir ( dir_path_obj )
     # read each page
-    for pg in range (0 , pdf . pageCount ) :
-    page = pdf [ pg ]
-    # zoom_x = zoom_y =3 -> use 3 * original resolution
-    trans = fitz . Matrix ( zoom_x , zoom_y ) . preRotate (
-    rotation_angle )
-    pm = page . getPixmap ( matrix = trans , alpha = False )
-    rename = pg + len ( files )
-    # write the image
-    pm . writePNG ( imgPath +str ( rename ) +".png")
-    pdf . close ()
+    for pg in range (0 , pdf.pageCount ) :
+        page = pdf [ pg ]
+        # zoom_x = zoom_y =3 -> use 3 * original resolution
+        trans = fitz.Matrix ( zoom_x , zoom_y ).preRotate (rotation_angle)
+        pm = page.getPixmap ( matrix = trans , alpha = False )
+        rename = pg + len ( files )
+        # write the image
+        pm.writePNG ( imgPath +str ( rename ) +".png")
+    pdf.close ()
 
 def main ( _argv ) :
     # folder storing the PDF files
     dir_path = './ Dataset '
-    files = os . listdir ( dir_path )
+    files = os.listdir ( dir_path )
     for file in files :
-    pdfPath = dir_path +'/'+ file
-    # print ( pdfPath )
-    pdf_image ( pdfPath , './ Dataset / images /', 3 , 3 , 0)
+        pdfPath = dir_path +'/'+ file
+        # print ( pdfPath )
+        pdf_image ( pdfPath , './ Dataset / images /', 3 , 3 , 0)
     print (" finished ")
 
 if __name__ == '__main__':
